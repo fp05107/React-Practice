97dd990e9d13a727e18de7046367c9cb39c2a598
subham <andharalauri@gmail.com>
2022-12-21 21:28:10 +0530

    added

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import Textform from './components/Textform';
+import Navbar from './components/Navbar';
+import About from './components/About';
+import React,{useState} from 'react'
+import Alert from './components/Alert';
+
 
 function App() {
+  const [mode,setMode] = useState('light'); //Whether dark mode is enabled or not
+
+  let toggleMode = ()=>{
+    if(mode == 'light'){
+      setMode('dark')
+      document.body.style.backgroundColor = 'grey'
+    }else{
+      setMode('light')
+      document.body.style.backgroundColor = 'white'
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+     <Alert alert='This is alert' />
+    <Navbar title='TextUtils' mode={mode} toggleMode={toggleMode} />
+    <div className="container my-3">
+
+    <Textform heading = "Enter Text Here" />
+    <About/>
+   
     </div>
+    </>
   );
 }
 
 export default App;
+
---
+++ my-app/src/components/Textform.js
@@ -0,0 +1,58 @@
+import React, { useState } from "react";
+
+export default function Textform(props) {
+    const [text, setText] = useState("");
+    // setText('enter lund here');
+    // console.log(text);
+    let changeToUppercase = () => {
+        let y = text.toUpperCase();
+        setText(y);
+    };
+    let changeToLowercase = ()=>{
+        setText(text.toLowerCase());
+    }
+    let handleOnChange = (event) => {
+        console.log("handle on change");
+        setText(event.target.value);
+    };
+    let handleCopy = ()=>{
+        let text = document.getElementById("myBox");
+        text.select();
+        navigator.clipboard.writeText(text.value);
+    }
+    return (
+        <>
+            <div className="container">
+                <h1>{props.heading}</h1>
+                <div className="mb-3">
+                    <textarea
+                        className="form-control my-3"
+                        id="myBox"
+                        value={text}
+                        onChange={handleOnChange}
+                        rows="10"
+                    ></textarea>
+                    <button className="btn btn-primary mx-3" onClick={changeToUppercase}>
+                        Convert to Uppercase
+                    </button>
+                    <button className="btn btn-primary mx-3" onClick={changeToLowercase}>
+                        Convert to Lowercase
+                    </button>
+                    <button className="btn btn-primary" onClick={handleCopy}>
+                        Copy 
+                    </button>
+                </div>
+            </div>
+
+            <div className="container my-3">
+                <h1>Your Text Summary</h1>
+                <p>{text.split(" ").length}word and {text.length} characters</p>
+                <p>{0.008*text.split(" ").length +"Minutes To Read"}</p>
+                <h2>Preview</h2>
+                <p>{text.length>0?text:'Enter Text To Preview It Here'}</p>
+
+
+            </div>
+        </>
+    );
+}
